264c756ac7249ae478406130daf35c1caf187945
Serge Hänni <serge@nyi.ch>
2020-01-01 17:36:23 +0100

    Change to load level from file

--- level.rb
+++ level.rb
@@ -1,4 +1,5 @@
 require 'matrix'
+require 'yaml'
 
 class Level
   WALL = 97
@@ -8,29 +9,27 @@ class Level
 
   TILE_SIZE = 64
 
-  MAP = [
-    [WALL, WALL, WALL, WALL],
-    [WALL, GROUND, GROUND, WALL],
-    [WALL, BOX, BOX, WALL],
-    [WALL, GROUND, GROUND, WALL],
-    [WALL, GOAL, GOAL, WALL],
-    [WALL, WALL, WALL, WALL]
-  ]
-
   def initialize
     @sprites = Gosu::Image.load_tiles 'assets/kenney_sokobanpack/Tilesheet/sokoban_tilesheet.png', TILE_SIZE, TILE_SIZE
     @player = Player.new(2, 1)
+
+    @map = YAML::load_file('levels/0.yml')
+    @map = @map.map do |row|
+      row.map do |cell|
+        eval(cell.to_s.upcase)
+      end
+    end
   end
 
   def draw
     # Draw ground everywhere first, to cover eventual holes
-    MAP.each_with_index do |array, row|
+    @map.each_with_index do |array, row|
       array.each_with_index do |tile_type, column|
         @sprites[GROUND].draw column * TILE_SIZE, row * TILE_SIZE, 1
       end
     end
 
-    MAP.each_with_index do |array, row|
+    @map.each_with_index do |array, row|
       array.each_with_index do |tile_type, column|
         @sprites[tile_type].draw column * TILE_SIZE, row * TILE_SIZE, 1
       end
@@ -53,7 +52,7 @@ class Level
       Vector[@player.x, @player.y]
     end
 
-    case MAP[target[1]][target[0]]
+    case @map[target[1]][target[0]]
     when GROUND
       @player.x = target[0]
       @player.y = target[1]
@@ -61,18 +60,18 @@ class Level
       push_vector = Vector[target[0] - @player.x, target[1] - @player.y]
       box_target = target + push_vector
 
-      case MAP[box_target[1]][box_target[0]]
+      case @map[box_target[1]][box_target[0]]
       when GROUND, GOAL
         @player.x = target[0]
         @player.y = target[1]
 
-        MAP[box_target[1]][box_target[0]] = BOX
-        MAP[target[1]][target[0]] = GROUND
+        @map[box_target[1]][box_target[0]] = BOX
+        @map[target[1]][target[0]] = GROUND
       end
     end
   end
 
   def solved?
-    !MAP.flatten.include?(GOAL)
+    !@map.flatten.include?(GOAL)
   end
 end
