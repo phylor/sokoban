9ecd1c0a410459bdb249da5c299d2b32d38c1b08
Serge Hänni <serge@nyi.ch>
2020-01-02 13:33:44 +0100

    Remove unnecessary loading of sprites

--- player.rb
+++ player.rb
@@ -1,18 +1,14 @@
 require_relative './coordinates'
 
 class Player
-  TILE_SIZE = 64
-
   attr_accessor :position
   attr_accessor :undos
 
   def initialize
-    @sprites = Gosu::Image.load_tiles 'assets/kenney_sokobanpack/Tilesheet/sokoban_tilesheet.png', TILE_SIZE, TILE_SIZE
-
     @undos = 3
   end
 
   def draw
-    @sprites[65].draw position[0] * Sprites::TILE_SIZE, position[1] * Sprites::TILE_SIZE, 2
+    Sprites.instance[Sprites::PLAYER].draw position[0] * Sprites::TILE_SIZE, position[1] * Sprites::TILE_SIZE, 2
   end
 end
--- sprites.rb
+++ sprites.rb
@@ -11,6 +11,8 @@ class Sprites
   GOAL = 25
   NOTHING = 0
 
+  PLAYER = 65
+
   def initialize
     @@sprites = Gosu::Image.load_tiles 'assets/kenney_sokobanpack/Tilesheet/sokoban_tilesheet.png', TILE_SIZE, TILE_SIZE
     @@misc = Gosu::Image.load_tiles 'assets/sheet_white1x.png', 50, 50
